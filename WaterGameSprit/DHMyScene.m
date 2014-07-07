12f99a53ed5205547d5526ccb7c05ba15002f132
Ahmed Hamdy <dimo.hamdy@gmail.com>
2014-07-08 01:48:42 +0200

    push Button
    add right and left button

--- WaterGameSprit/DHMyScene.m
+++ WaterGameSprit/DHMyScene.m
@@ -30,57 +30,94 @@
 //                                       CGRectGetMidY(self.frame));
 //        
 //        [self addChild:myLabel];
-        SKSpriteNode *ring = [SKSpriteNode spriteNodeWithImageNamed:@"ring"];
-        
-        ring.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));;
-        
-        ring.physicsBody=[SKPhysicsBody bodyWithCircleOfRadius:ring.size.width/2]; // you can change the radius
-        
-        ring.physicsBody.dynamic = YES;
-        
-        
-        ring.physicsBody.affectedByGravity=YES;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
 
-        [ring runAction:[SKAction repeatActionForever:action]];
         
-        [self addChild:ring];
+        for (int count=0; count<10; count++) {
+            
+            
+            
+            [self addChild:[self setupRing:count]];
+
+        }
+        
+        [self addChild:[self rightButtonNode]];
+        [self addChild:[self leftButtonNode]];
+
+
     }
     return self;
 }
+-(SKSpriteNode*)setupRing:(int)count{
+    SKSpriteNode *ring = [SKSpriteNode spriteNodeWithImageNamed:@"ring"];
+    
+    ring.position = CGPointMake(CGRectGetMidX(self.frame)+count*5, CGRectGetMidY(self.frame)+count*5);;
+    
+    ring.physicsBody=[SKPhysicsBody bodyWithCircleOfRadius:ring.size.width/2]; // you can change the radius
+    ring.physicsBody.dynamic = YES;
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *ring = [SKSpriteNode spriteNodeWithImageNamed:@"ring"];
-        
-        ring.position = location;
-        ring.physicsBody=[SKPhysicsBody bodyWithCircleOfRadius:ring.size.width/2]; // you can change the radius
-        
-        ring.physicsBody.dynamic = YES;
-        
-        
-        ring.physicsBody.affectedByGravity=YES;
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [ring runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:ring];
-        
-        
-        
-    }
+    ring.physicsBody.affectedByGravity=YES;
     
+    SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
     
-    
+    [ring runAction:[SKAction repeatActionForever:action]];
+   
+    return ring;
 }
 
--(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
+
+//Left button
+- (SKSpriteNode *)leftButtonNode
+{
+    SKSpriteNode *fireNode = [SKSpriteNode spriteNodeWithImageNamed:@"push"];
+    //fireNode.physicsBody=[SKPhysicsBody bodyWithCircleOfRadius:fireNode.size.width/2]; // you can change the radius
+
+    fireNode.position = CGPointMake(15, 30);
+    fireNode.name = @"fireButtonNode";//how the node is identified later
+    fireNode.zPosition = 1.0;
+    return fireNode;
+}
+//Right button
+- (SKSpriteNode *)rightButtonNode
+{
+    SKSpriteNode *fireNode = [SKSpriteNode spriteNodeWithImageNamed:@"push"];
+   // fireNode.physicsBody=[SKPhysicsBody bodyWithCircleOfRadius:fireNode.size.width/2]; // you can change the radius
+
+    fireNode.position = CGPointMake(300, 30);
+    fireNode.name = @"fireButtonNode";//how the node is identified later
+    fireNode.zPosition = 1.0;
+    return fireNode;
+}
+
+////handle touch events
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInNode:self];
+    SKNode *node = [self nodeAtPoint:location];
+    
+    //if fire button touched, bring the rain
+    if ([node.name isEqualToString:@"fireButtonNode"]) {
+        //do whatever...
+        self.physicsWorld.gravity = CGVectorMake(+0.0f, +1.8f); // setting the gravity of the scene.
+        [self setPhysicsBody:[SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame]];
+        [self performSelector:@selector(resetGravity) withObject:nil afterDelay:1];
+    }
+}
+-(void)resetGravity{
+    self.physicsWorld.gravity = CGVectorMake(+0.0f, -1.8f); // setting the gravity of the scene.
+    [self setPhysicsBody:[SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame]];  //Physics body of Scene
+
+}
+- (void)didEndContact:(SKPhysicsContact *)contact{
+    SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+    
+    [contact.bodyA.node runAction:[SKAction repeatAction:action count:1]];
+}
+- (void)didBeginContact:(SKPhysicsContact *)contact{
+    SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+    
+    [contact.bodyA.node runAction:[SKAction repeatAction:action count:1]];
 }
 
 @end
